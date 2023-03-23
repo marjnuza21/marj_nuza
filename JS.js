65be3a5ae7c536f4c58eec863d310e81c48f251c
Marj Nuza <92004444+marjnuza21@users.noreply.github.com>
2023-03-22 20:32:10 -0600

    Update JS.js

--- JS.js
+++ JS.js
@@ -15,10 +15,6 @@ function OpenHome() {
  function OpenMilestone() {
   location.href = "milestone.html"
  }
-
- function OpenFFT() {
-  location.href = "home_onepage.html"
- }
  
  function hide() {
   document.getElementById("lets_connect").style.display ="none"
@@ -33,29 +29,188 @@ function OpenHome() {
 
 //This is for NotABot//
 var i = 0;
-var text = "Hey there. Hope you are doing alright.\n\nI am Marj, a software engineer with a background in business and cybersecurity.\n\nI'm highly involved in back-end engineering in my day job but I've always been passionate and interested in developing my front-end engineering skills. This is why I've decided to build my professional portfolio website from scratch.\n\nI aspire to become an instrument to inspire other people with my work and provide value to other people's lives even in the simpliest way possible.\n\nThank you for visiting my site. :) Feel free to explore my page :)"
+var human = "Hey there. Hope you are doing alright.\n\nI am Marj, a software engineer with a background in business and cybersecurity.\n\nI'm highly involved in back-end engineering in my day job but I've always been passionate and interested in developing my front-end engineering skills.\n\nI also enjoy traveling and seeing other places, meeting new people, and understanding other people's cultures.\nI aspire to become an instrument to inspire other people with my work and provide value to other people's lives even in the simpliest way possible.\n\nThank you for visiting my site. :) Feel free to explore my page :)"
 ;
-var yesBot = "Sorry Robot. Better luck next time!!!";
-var visitor = "Please confirm that you're not a bot."
-var speed = 50;
+var robot = "\n\nSorry Robot.\n\nBetter luck next time!!!";
+var ghost = "\n\nYou're a ghost.\n\nGo away!!!";
+var square = "\n\nDon't be a square.";
+var redflag = "\n\nI'm hoping to see the red flag as early as possible,\nbut not today.";
+var puppy = "\n\nWooofff wooofff.\n\nHi puppy, I'm sure you're adorable, \n\nbut I don't have a treat for you today :(";
+var zombie = "\n\nIs it zombie apocalypse yet?";
+var clown = "\n\nThere are lots of clowns \n\nwandering out there.\n\nAre you one of them?";
+var cake = "\n\nWho doesn't like a piece of cake?\n\nI'll take it :)";
+var speed = 70;
 
-function NotABot() {
-if ( i < text.length) {
-  document.getElementById("content").innerHTML +=text.charAt(i);
+function ItsAHuman() {
+if ( i < human.length) {
+  document.getElementById("content").innerHTML +=human.charAt(i);
   i++;
-  setTimeout(NotABot, speed)
+  setTimeout(ItsAHuman, speed)
 }
-document.getElementById("yes").style.display ="none"
+document.getElementById("tryagain").style.display ="none"
+document.getElementById("proof").style.display ="none"
+document.getElementById("robot").style.display ="none"
+document.getElementById("ghost").style.display ="none"
+document.getElementById("square").style.display ="none"
+document.getElementById("redflag").style.display ="none"
+document.getElementById("human").style.display ="none"
+document.getElementById("puppy").style.display ="none"
+document.getElementById("zombie").style.display ="none"
+document.getElementById("clown").style.display ="none"
+document.getElementById("cake").style.display ="none"
 }
 
 function ItsABot() {
-  if ( i < yesBot.length) {
-    document.getElementById("content").innerHTML +=yesBot.charAt(i);
+  if ( i < robot.length) {
+    document.getElementById("content").innerHTML +=robot.charAt(i);
     i++;
     setTimeout(ItsABot, speed)
   }
-  document.getElementById("no").style.display ="none"
+  document.getElementById("tryagain").style.display ="block"
+  document.getElementById("proof").style.display ="none"
+  document.getElementById("robot").style.display ="none"
+  document.getElementById("ghost").style.display ="none"
+  document.getElementById("square").style.display ="none"
+  document.getElementById("redflag").style.display ="none"
+  document.getElementById("human").style.display ="none"
+  document.getElementById("puppy").style.display ="none"
+  document.getElementById("zombie").style.display ="none"
+  document.getElementById("clown").style.display ="none"
+  document.getElementById("cake").style.display ="none"
+}
+
+function ItsAGhost() {
+  if ( i < ghost.length) {
+    document.getElementById("content").innerHTML +=ghost.charAt(i);
+    i++;
+    setTimeout(ItsAGhost, speed)
   }
+  document.getElementById("tryagain").style.display ="block"
+  document.getElementById("proof").style.display ="none"
+  document.getElementById("robot").style.display ="none"
+  document.getElementById("ghost").style.display ="none"
+  document.getElementById("square").style.display ="none"
+  document.getElementById("redflag").style.display ="none"
+  document.getElementById("human").style.display ="none"
+  document.getElementById("puppy").style.display ="none"
+  document.getElementById("zombie").style.display ="none"
+  document.getElementById("clown").style.display ="none"
+  document.getElementById("cake").style.display ="none"
+}
+
+function ItsASquare() {
+  if ( i < square.length) {
+    document.getElementById("content").innerHTML +=square.charAt(i);
+    i++;
+    setTimeout(ItsASquare, speed)
+  }
+  document.getElementById("tryagain").style.display ="block"
+  document.getElementById("proof").style.display ="none"
+  document.getElementById("robot").style.display ="none"
+  document.getElementById("ghost").style.display ="none"
+  document.getElementById("square").style.display ="none"
+  document.getElementById("redflag").style.display ="none"
+  document.getElementById("human").style.display ="none"
+  document.getElementById("puppy").style.display ="none"
+  document.getElementById("zombie").style.display ="none"
+  document.getElementById("clown").style.display ="none"
+  document.getElementById("cake").style.display ="none"
+}
+
+function ItsARedFlag() {
+  if ( i < redflag.length) {
+    document.getElementById("content").innerHTML +=redflag.charAt(i);
+    i++;
+    setTimeout(ItsARedFlag, speed)
+  }
+  document.getElementById("tryagain").style.display ="block"
+  document.getElementById("proof").style.display ="none"
+  document.getElementById("robot").style.display ="none"
+  document.getElementById("ghost").style.display ="none"
+  document.getElementById("square").style.display ="none"
+  document.getElementById("redflag").style.display ="none"
+  document.getElementById("human").style.display ="none"
+  document.getElementById("puppy").style.display ="none"
+  document.getElementById("zombie").style.display ="none"
+  document.getElementById("clown").style.display ="none"
+  document.getElementById("cake").style.display ="none"
+}
+
+function ItsAPuppy() {
+  if ( i < puppy.length) {
+    document.getElementById("content").innerHTML +=puppy.charAt(i);
+    i++;
+    setTimeout(ItsAPuppy, speed)
+  }
+  document.getElementById("tryagain").style.display ="block"
+  document.getElementById("proof").style.display ="none"
+  document.getElementById("robot").style.display ="none"
+  document.getElementById("ghost").style.display ="none"
+  document.getElementById("square").style.display ="none"
+  document.getElementById("redflag").style.display ="none"
+  document.getElementById("human").style.display ="none"
+  document.getElementById("puppy").style.display ="none"
+  document.getElementById("zombie").style.display ="none"
+  document.getElementById("clown").style.display ="none"
+  document.getElementById("cake").style.display ="none"
+}
+
+function ItsAZombie() {
+  if ( i < zombie.length) {
+    document.getElementById("content").innerHTML +=zombie.charAt(i);
+    i++;
+    setTimeout(ItsAZombie, speed)
+  }
+  document.getElementById("tryagain").style.display ="block"
+  document.getElementById("proof").style.display ="none"
+  document.getElementById("robot").style.display ="none"
+  document.getElementById("ghost").style.display ="none"
+  document.getElementById("square").style.display ="none"
+  document.getElementById("redflag").style.display ="none"
+  document.getElementById("human").style.display ="none"
+  document.getElementById("puppy").style.display ="none"
+  document.getElementById("zombie").style.display ="none"
+  document.getElementById("clown").style.display ="none"
+  document.getElementById("cake").style.display ="none"
+}
+
+function ItsAClown() {
+  if ( i < clown.length) {
+    document.getElementById("content").innerHTML +=clown.charAt(i);
+    i++;
+    setTimeout(ItsAClown, speed)
+  }
+  document.getElementById("tryagain").style.display ="block"
+  document.getElementById("proof").style.display ="none"
+  document.getElementById("robot").style.display ="none"
+  document.getElementById("ghost").style.display ="none"
+  document.getElementById("square").style.display ="none"
+  document.getElementById("redflag").style.display ="none"
+  document.getElementById("human").style.display ="none"
+  document.getElementById("puppy").style.display ="none"
+  document.getElementById("zombie").style.display ="none"
+  document.getElementById("clown").style.display ="none"
+  document.getElementById("cake").style.display ="none"
+}
+
+function ItsACake() {
+  if ( i < cake.length) {
+    document.getElementById("content").innerHTML +=cake.charAt(i);
+    i++;
+    setTimeout(ItsACake, speed)
+  }
+  document.getElementById("tryagain").style.display ="block"
+  document.getElementById("proof").style.display ="none"
+  document.getElementById("robot").style.display ="none"
+  document.getElementById("ghost").style.display ="none"
+  document.getElementById("square").style.display ="none"
+  document.getElementById("redflag").style.display ="none"
+  document.getElementById("human").style.display ="none"
+  document.getElementById("puppy").style.display ="none"
+  document.getElementById("zombie").style.display ="none"
+  document.getElementById("clown").style.display ="none"
+  document.getElementById("cake").style.display ="none"
+}
 
 function askVisitor() {
     if ( i < visitor.length) {
@@ -63,5 +218,5 @@ function askVisitor() {
       i++;
       setTimeout(askVisitor, speed)
     }
-    }
+}
 
